bb6447932599cfb03339f1f2efa75072c48ac377
Preeti <prity.dudi@gmail.com>
2024-08-30 10:27:23 +0530

    db port

--- back-end/src/config/database.ts
+++ back-end/src/config/database.ts
@@ -9,8 +9,8 @@ const sequelize = new Sequelize(
   process.env.DB_PASS!,
   {
     host: process.env.DB_HOST,
-    port: Number(process.env.PORT) ,
-    dialect: 'mysql',
+    port: Number(process.env.DB_PORT) ,
+    dialect: 'mysql'
   }
 );
 
