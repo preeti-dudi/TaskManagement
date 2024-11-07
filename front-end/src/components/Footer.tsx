65a1a1dd281b0a389b9fa6dfc06280372bf9e6a0
Preeti <prity.dudi@gmail.com>
2024-11-07 14:22:18 +0530

    toggle button update

--- front-end/src/components/Footer.tsx
+++ front-end/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
 import { Form, Input, Button, message, Row, Col } from 'antd';
-import { FacebookOutlined, TwitterOutlined, InstagramOutlined, LinkedinOutlined, MailOutlined, PhoneOutlined, GlobalOutlined, GithubOutlined } from '@ant-design/icons';
+import {InstagramOutlined, LinkedinOutlined, GlobalOutlined, GithubOutlined } from '@ant-design/icons';
 import './Footer.css';
 
 const ContactForm: React.FC = () => {
